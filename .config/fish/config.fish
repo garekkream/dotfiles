c03c85859c93a42b127e3c195bbbc136e93baaa3
Krzysztof Garczynski <krzysztof.garczynski@gmail.com>
2017-10-03 20:48:00 +0200

    Updated configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ set -x GOPATH $HOME/.go
 set -x EDITOR "vim"
 set -x VISUAL "gvim"
 
+set -g -x BIN_PATH /opt/esp-open-sdk/xtensa-lx106-elf/bin/
+set -g -x SDK_PATH /opt/esp-open-sdk/sdk/
+
 alias gvim "gvim -p --remote-tab-silent"
 alias ls "ls --color=auto"
 alias ll "ls --color=auto -thor"
@@ -12,7 +15,7 @@ alias svim "sudo vim"
 alias grep "grep --color=always"
 alias top "htop"
 
-alias sshValkiria "ssh bananapi@Valkiria -X"
+alias sshValkiria "ssh garekkream@Valkiria -X"
 
 ulimit -c unlimited
 
@@ -20,3 +23,4 @@ function su
     /bin/su --shell=/usr/bin/fish $argv
 end
 set fish_greeting
+
